11284398c91842b85c9ce0923f8bf90c0c99f1d4
DinhLuong04 <166143726+DinhLuong04@users.noreply.github.com>
2025-11-08 23:53:03 +0700

    feat: complete full enemy system with prefab, logic, animations, and particle effects 8/11/2025

--- Assets/Resources/MyGame/Scripts/Enemies/EnemyType.cs
+++ Assets/Resources/MyGame/Scripts/Enemies/EnemyType.cs
@@ -2,4 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EnemyType { Frog, Bat, Slime  }
+public enum EnemyType
+{
+    Frog, Bat, LaughingBubbles, Samurai, Sanosuke,
+    Bee, Eagle, Mosquito, MutantButterfly, StrangeBeast,
+    BoneDevil, DemonSwordsman, MudDevil, MutantPig, OneEye,
+    Pig, PigKing, Piranha, PiranhaRed, PupleSpider, SpottedSpider,
+    ScorpionRed,Snail, Turtle,Snake,SwordMan_1, SwordMan_2,
+
+}
